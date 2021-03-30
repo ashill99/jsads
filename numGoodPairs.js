d2f39d558bb051f97e42e56a86cab43b28a7a22c
ashill99 <admshng@gmail.com>
2021-03-30 12:52:11 -0400

    tues morn

---
+++ numGoodPairs.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var numIdenticalPairs = function(nums) {
+    
+    for(let i = 0; i < nums.length; i++) {
+        for(let j = 0; nums.length; j++) {
+            
+        }
+    }
+};
+
+console.log(numIdenticalPairs([1,2,3,1,1,3]))
