9e7f66a663f611dc6cc6d25804907d48eda2efbc
ashill99 <admshng@gmail.com>
2021-06-13 21:28:38 +0100

    morse sol

---
+++ uniqueMorse.js
@@ -0,0 +1,54 @@
+var uniqueMorseRepresentations = function(words) {
+    
+	// Define a set and variable morse (set to empty string);
+    let morseSet = new Set;
+    let morse = "";
+    
+	// Define object literal
+    function decypher(elem) {
+        var code = {
+            "a": ".-",
+            "b":"-...",
+            "c":"-.-.",
+            "d":"-..",
+            "e":".",
+            "f":"..-.",
+            "g":"--.",
+            "h":"....",
+            "i":"..",
+            "j":".---",
+            "k":"-.-",
+            "l":".-..",
+            "m":"--",
+            "n":"-.",
+            "o":"---",
+            "p":".--.",
+            "q":"--.-",
+            "r":".-.",
+            "s":"...",
+            "t":"-",
+            "u":"..-",
+            "v":"...-",
+            "w":".--",
+            "x":"-..-",
+            "y":"-.--",
+            "z":"--.."
+        };    
+        return (code[elem]);
+    }
+    
+	// Iterate through each character in each word
+    for(let i = 0; i < words.length; i++) {
+        for(let j = 0; j < words[i].length; j++) {
+            morse += decypher(words[i][j]);
+        }
+		// Add to set
+        morseSet.add(morse);
+		
+		// Reset morse variable
+        morse = "";
+    }
+
+	// Return size of set 
+    return morseSet.size;
+};
